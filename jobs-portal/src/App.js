2030d6c31cc04b53f5fdb62cf7c5d90b20b07e3f
donbossco1432 <donbossco1432@gmail.com>
2025-05-14 13:52:02 +1000

    sd

--- jobs-portal/src/App.js
+++ jobs-portal/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link, useNavigate } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import { FaWhatsapp, FaBriefcase } from 'react-icons/fa';
